aaaf7c174edefa993335030b7259079688c958c0
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-03-27 13:16:18 +0530

    Update chef_config.sh

--- vinnet/chef_config.sh
+++ vinnet/chef_config.sh
@@ -1,4 +1,4 @@
-ip=$(head -25 /var/jenkins_home/workspace/Terraform-VM/vinnet/vm-output.json | tail -1 | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+ip=$(head -25 /var/jenkins_home/workspace/InfrastructurePipeline/vinnet/vm-output.json | tail -1 | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
 ssh -t 'root@40.83.187.131' 'sshpass -f password.txt ssh-copy-id vineet@'$ip''
 #ssh -t  vineet@$ip
 ssh -t 'root@40.83.187.131' ip="$ip" bash -s<<'SSH_EOF'
