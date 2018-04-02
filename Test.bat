bfe01a792d84560b25da8602f85d496a9a0db83b
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-04-02 16:56:03 +0530

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -5,8 +5,5 @@ cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="300"}
 cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
 cmd.exe /c winrm set winrm/config/client/auth @{Basic="true"}
-cmd.exe /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
 cmd.exe /c netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" profile=public protocol=tcp localport=5985 remoteip=localsubnet new remoteip=any
-cmd.exe /c net stop winrm 
-cmd.exe /c sc config winrm start= auto
-cmd.exe /c net start winrm 
+
