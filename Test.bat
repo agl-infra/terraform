0c43eac4f208e2581abb42e621c33999ed6dd92b
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-04-02 16:41:49 +0530

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,4 +1,4 @@
-cmd.exe /c winrm quickconfig -q
+winrm quickconfig -q
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" profile=public protocol=tcp localport=5985 remoteip=localsubnet new remoteip=any
